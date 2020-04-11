d33b81cc30a0c3cdfd1c24e4355040fbdc1d0ba7
AlexArcticFox <alextromb1@gmail.com>
2020-04-11 18:23:34 +0200

    Trackscheduler

--- src/SymphonyClient.ts
+++ src/SymphonyClient.ts
@@ -3,18 +3,15 @@ import ConfigTemplate from "~/Config";
 import { IFunctionResult } from "~/ConfigHandler";
 import CommandHandler from "@command/CommandHandler";
 import Database from "@database/Database";
-import { Shoukaku, ShoukakuNodeOptions } from "shoukaku";
 
 export default class SymphonyClient extends Client {
     readonly config: IFunctionResult<typeof ConfigTemplate>;
     readonly database: Database;
-    readonly shoukaku: Shoukaku;
 
     constructor(config: IFunctionResult<typeof ConfigTemplate>, database: Database, options?: ClientOptions) {
         super(options);
         this.config = config;
         this.database = database;
-        this.shoukaku = new Shoukaku(this, config.lavalink as ShoukakuNodeOptions[], { moveOnDisconnect: true, resumable: false, reconnectTries: 2, restTimeout: 10000 });
         this.once("ready", () => {
             new CommandHandler (this)
         });
--- src/TrackScheduler.ts
+++ src/TrackScheduler.ts
@@ -1,10 +1,99 @@
-/*import { Guild } from "discord.js";
+import { Client } from "discord.js";
+import ConfigTemplate from "~/Config";
+import { IFunctionResult } from "~/ConfigHandler";
+import { Shoukaku, ShoukakuNodeOptions } from "shoukaku";
 
 export default class TrackScheduler {
-    readonly guild: Guild;
-    private readonly queue: any[];
+    readonly shoukaku: Shoukaku;
+    private readonly queues: Map<string, object[]> = new Map();
 
-    constructor(guild: Guild) {
-        this.guild = guild;
+    constructor(client: Client, config: IFunctionResult<typeof ConfigTemplate>) {
+        this.shoukaku = new Shoukaku(client, config.lavalink as ShoukakuNodeOptions[], { moveOnDisconnect: true, resumable: false, reconnectTries: 2, restTimeout: 10000 });
+        this.shoukaku.on('ready', (name) => {
+            console.log(`Lavalink Node: ${name} is now connected`)
+        });
+        this.shoukaku.on('error', (name, error) => {
+            console.log(`Lavalink Node: ${name} emitted an error.`, error)
+        });
+        this.shoukaku.on('close', (name, code, reason) => {
+            console.log(`Lavalink Node: ${name} closed with code ${code}. Reason: ${reason || 'No reason'}`)
+        });
+        this.shoukaku.on('disconnected', (name, reason) => {
+            console.log(`Lavalink Node: ${name} disconnected. Reason: ${reason || 'No reason'}`)
+        });
     }
-}*/
+
+    getQueue(guildId: string): object[] {
+        return this.queues.get(guildId) ?? [];
+    }
+    
+    addSong(guildId: string, song: object) {
+        this.queues.set(guildId, [...this.getQueue(guildId), song]);
+    }
+    
+    removeSong(queue: object[], index: number): object[];
+    removeSong(guildId: string, index: number): boolean;
+    removeSong(queue: object[], song: object): object[] | null;
+    removeSong(guildId: string, song: object): boolean;
+    removeSong(queueOrGuildId: string | object[], indexOrSong: number|object): boolean | object[] | null {
+        if (typeof queueOrGuildId === "string") {
+            if (typeof indexOrSong === "number") {
+                this.queues.set(queueOrGuildId, this.removeSong(this.getQueue(queueOrGuildId), indexOrSong));
+                return true;
+            }
+            else {
+                return this.removeSong(this.getQueue(queueOrGuildId), indexOrSong) !== null
+            }
+        }
+        else {
+            if (typeof indexOrSong === "number") {
+                queueOrGuildId.splice(indexOrSong, 1);
+                return queueOrGuildId;
+            }
+            else {
+                const index = queueOrGuildId.indexOf(indexOrSong);
+                
+                if (index === -1) {
+                    return null;
+                }
+                
+                return this.removeSong(queueOrGuildId, indexOrSong);
+            }
+        }
+    }
+    
+    removeSongs(guildId: string, indexes: number[]): boolean;
+    removeSongs(guildId: string, songs: object[]): object[];
+    removeSongs(guildId: string, indexesOrSongs: number[] | object[]): boolean | object[] {
+        if (indexesOrSongs.length === 0) {
+            throw Error("Empty arrays are not allowed");
+        }
+        if (typeof indexesOrSongs === "number") {
+            this.queues.set(guildId, this.getQueue(guildId).filter((_, i) => !(indexesOrSongs as number[]).includes(i)));
+            return true;
+        }
+        else {
+            let queue = [...this.getQueue(guildId)];
+            const missing = [] as Object[];
+            
+            indexesOrSongs.forEach((song: Object) => {
+                song as Object;
+                const newQueue = this.removeSong(queue, song);
+                
+                if (newQueue === null) {
+                    missing.push(song);
+                } else {
+                    queue = newQueue;
+                }
+            })
+            
+            if (missing.length === 0) {
+                return missing;
+            }
+            
+            this.queues.set(guildId, queue);
+            
+            return [];
+        }
+    }
+}
